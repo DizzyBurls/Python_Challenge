5865570575b584c155892c4677dce295ee7b9119
Chris Burley <chrisjburley@gmail.com>
2022-02-22 14:40:15 +1100

    Lists all candidates once. Calculates votes when given candidate name. Calculates total votes.

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,6 +7,8 @@ csvpath = os.path.join(r'C:\Users\chris\Documents\Data Analytics Bootcamp\Homewo
 VoterID=[]
 County=[]
 Candidate=[]
+OverallVotes=[]
+DifferentCandidates = []
 
 SelectCandidate = input("Whose election performance would you like to see? ")
 
@@ -14,20 +16,32 @@ found = False
 
 with open(csvpath, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+
+    # Skip the headers when reading the source file.
     
     csv_header = next(csvfile)
+
+    # Print the headers that exist in the sources file.
+
     print(f"Header: {csv_header}")
 
     for row in csvreader:
+        OverallVotes.append(row[2])
+        
         if (row[2]) == SelectCandidate:
-            print(row[2] + " secured the vote of " + row[0] + " from " + row[1] + ".")
+            #print(row[2] + " secured the vote of " + row[0] + " from " + row[1] + ".")
             found = True
             Candidate.append(row[2])
             County.append(row[1])
             VoterID.append(row[0])
 
-if found is False:
+        if (row[2]) not in DifferentCandidates:
+            DifferentCandidates.append(row[2])
+
+    
+    if found is False:
         print("No candidate matches that description.")
+
 #print(Candidate)
 cleaned_election_csv = zip(VoterID, County, Candidate)
 
@@ -39,6 +53,15 @@ with open(output_path, "w", newline='') as csvfile:
     writer.writerow(['Voter_ID', 'County', 'Candidate'])
 
     writer.writerows(cleaned_election_csv)
+
+# Print the number of votes the candidate received.
+
+print(len(DifferentCandidates))
+print(DifferentCandidates)
+print(len(Candidate))
+print(len(OverallVotes))
+
+
 
 
 
