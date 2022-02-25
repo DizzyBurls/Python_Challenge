38e9d6f664863fed9d88c8c9e719341a7bb054fd
Chris Burley <chrisjburley@gmail.com>
2022-02-26 10:57:45 +1100

    PyBank Writing to Text, PyPoll Complete.

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,6 +21,7 @@ MonthList=[]
 PandLList=[]
 DiffProf=[]
 
+
 with open(csvpath, encoding='utf') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
 
@@ -36,9 +37,10 @@ with open(csvpath, encoding='utf') as csvfile:
     
     NumMonths=len(MonthList)
     print(f"Total Months: {NumMonths}")
+    
 
 def sum(numbers):
-    Aggregate = 0.0
+    Aggregate = 0
     for number in numbers:
         Aggregate += number
     return Aggregate
@@ -46,16 +48,41 @@ def sum(numbers):
 # Test your function with the following:
 
 TotalPandL=sum(PandLList)
-print(f"Total Profit and Loss: {TotalPandL}") 
+print(f"Total Profit and Loss: ${TotalPandL}") 
 
+# Use a function to create a new list with the CHANGE in profit each month recoreded in it.
 def difference (MonthList):
     for i in range(1, len(MonthList)):
         DiffProf.append(MonthList[i] - MonthList[i-1])
-
+        
 difference(PandLList)
 
+# Use previously established SUM function to determine the average monthly change in profit.
 AverageProf=(sum(DiffProf))/(len(MonthList)-1)
-print(AverageProf)
+Limited_AverageProf=round(AverageProf, 2)
+print(f"Average Change: ${Limited_AverageProf}")
+
+MaxIncrease=DiffProf[0]
+iMax=0
+
+for i in range(1, len(MonthList)-1):
+        if MaxIncrease<=DiffProf[i]:
+            MaxIncrease=DiffProf[i]
+            iMax=i
+            
+
+print(f"Greatests increase in profits: {MonthList[iMax+1]} (${MaxIncrease})")
+
+
+MaxDecrease=DiffProf[0]
+iMin=0
+
+for i in range(1, len(MonthList)-1):
+        if MaxDecrease>=DiffProf[i]:
+            MaxDecrease=DiffProf[i]
+            iMin=i
+
+print(f"Greatests decrease in profits: {MonthList[iMin+1]} (${MaxDecrease})")
 
 file.close()
 
